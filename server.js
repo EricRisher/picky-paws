fe8910f1a03d77a21894cc81586186f4d6c333dd
EricRisher <rishereric13@gmail.com>
2023-11-09 21:55:27 -0800

    added server!

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+//const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//app.use(routes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
