8aa7e0708fe385e761070772a04234f412eec6bd
EricRisher <rishereric13@gmail.com>
2023-11-13 18:57:42 -0800

    added extra backend server files and index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 //app.use(routes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world" });
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 sequelize.sync({ force: false }).then(() => {
