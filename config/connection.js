529f9cc6553f58f1b4afa8b19d9dbafba4d61674
EricRisher <rishereric13@gmail.com>
2023-11-16 19:47:34 -0800

    fixed sql connection to heroku hopefully

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
+let sequelize;
+
 // create connection to our db
-const sequelize = new Sequelize(
+
+if (process.env.JAWSDB_URL) {
+  // If JawsDB is present (Heroku), use its URL
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: "mysql",
+  });
+} else {
+  // Use local database connection
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
     {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
     }
-);
+  );
+}
 
 module.exports = sequelize;
