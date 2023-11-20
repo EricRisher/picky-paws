9a1c258d6d01e10ce613518e5d2fb2da32d2478c
EricRisher <rishereric13@gmail.com>
2023-11-19 19:25:11 -0800

    errors

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require("./routes/apiRoutes");
 const sequelize = require('./config/connection');
 const path = require('path');
+const cors = require("cors");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,7 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes); 
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
