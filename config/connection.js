33b43d5ea5372a88c4c07d467db767d3c941d13e
EricRisher <rishereric13@gmail.com>
2023-11-19 19:29:56 -0800

    idk anymore

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const JAWSDB_URL =
+  "mysql://wee846igd6opyobo:l8m4ab7qlcnibwf2@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bd56pcqod4jd4z1b";
 
 let sequelize;
 
 // create connection to our db
 
-if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL || JAWSDB_URL) {
   // If JawsDB is present (Heroku), use its URL
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+  sequelize = new Sequelize(process.env.JAWSDB_URL || JAWSDB_URL, {
     dialect: "mysql",
   });
 } else {
